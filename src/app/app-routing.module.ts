64f3789aab4068c258ee2152993b741ddeb39510
rohitdandwani <rohitdandwani97@gmail.com>
2021-08-26 17:24:49 +0400

    Initial design and layout implemented

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home', loadChildren: () => import('./modules/home/home.module').then(m => m.HomeModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  { path: 'orders', loadChildren: () => import('./modules/orders/orders.module').then(m => m.OrdersModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
